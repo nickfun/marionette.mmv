a56c07e79ce13dee16c4d1fd252d79bb83c73bce
Nicholas Funnell <nick@nick.gs>
2013-03-03 15:32:31 -0800

    adding grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON('package.json'),
+		// paths
+		path_source: 'src/',
+		path_dest:	'build/',
+		
+		// tasks
+		uglify: {
+		}
+	});
+
+	// import the modules
+});
